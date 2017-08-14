282cac1cac8f212e62aec024ce3d3fda7c124702
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2017-08-14 00:35:46 -0400

    at a reasonable stopping point

--- pokesite/pokedex/urls.py
+++ pokesite/pokedex/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pokemon_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^compare/(?P<pokemon2>[0-9]+)(?P<pokemon1>[0-9]+)$', views.compare, name='compare'),
+
 ]
--- pokesite/pokedex/views.py
+++ pokesite/pokedex/views.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 from .models import *
 
 def index(request):
@@ -16,3 +16,11 @@ def detail(request, pokemon_id):
     species = get_object_or_404(PokemonSpecies, pk=pokemon_id)
     moves = Move.objects.filter(pokemonmove__pokemon=pokemon)
     return render(request, 'pokedex/detail.html', {'pokemon': pokemon, 'species': species, 'moves': moves})
+
+# def compare(request, pokemon1_id, pokemon2_id):
+#     pokemon1 = get_object_or_404(Pokemon, name=pokemon1_id)
+#     pokemon2 = get_object_or_404(Pokemon, name=pokemon2_id)
+#
+#     return render(request, 'pokedex/compare.html', {pokemon1: 'pokemon1', pokemon2: 'pokemon2'})
+#
+#     return HttpResponseRedirect(reverse('compare', args=(pokemon1.id, pokemon2.id,)))
